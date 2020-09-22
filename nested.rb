4683b513de97c9c759d450ff0ad8ff031ea65f2d
Thomas Garayua <tgarayua@gmail.com>
2020-09-22 16:04:51 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,8 +15,9 @@ def hopper
         :languages => ["C"]
       }
     }
-
+    return programmer_hash[:grace_hopper]
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -36,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+    return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -68,6 +69,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 	:yukihiro_matsumoto => {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+ 		  },
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -95,7 +100,7 @@ def changing_alan
         :languages => ["COBOL", "FORTRAN"]
       },
       :alan_kay => {
-        :known_for => "Object Orientation",
+        :known_for => "GUI",
         :languages => ["Smalltalk", "LISP"]
       },
       :dennis_ritchie => {
@@ -122,8 +127,8 @@ def adding_to_dennis
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => ["C", "Assembly"]
       }
     }
-
+return programmer_hash
 end
