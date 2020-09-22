ee0bd16611196f11cf16f67ff14afd11ec7e2d82
Thomas Garayua <tgarayua@gmail.com>
2020-09-22 15:16:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -17,7 +17,7 @@ def hopper
     }
 
 end
-return programmer_hash[grace_hopper]
+programmer_hash[grace_hopper]
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
